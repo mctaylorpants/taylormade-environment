3533fb5b4f34015f4e5e259bcce329c96fca408a
Alex Taylor <alex.taylor@clio.com>
2022-01-26 10:21:15 -0800

    This is causing codespaces to hang

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get -y update && sudo apt-get -y install neovim
 
 echo "Installing and configuring Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-nvim +PluginInstall +qall
+# nvim +PluginInstall +qall
 
 echo "Symlinking dotfiles..."
 ln -fs .config/nvim/init.vim ~/.config/nvim/init.vm
