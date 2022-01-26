99a0993939244e2a8cb60c96e018a6234898d007
Alex Taylor <alex.taylor@clio.com>
2022-01-26 09:12:44 -0800

    More setup.sh updates

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
+set -e
+
 echo "Installing neovim..."
-sudo apt install neovim
+sudo apt update && sudo apt install neovim
 
 echo "Installing and configuring Vundle..."
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 nvim +PluginInstall +qall
 
 echo "Symlinking dotfiles..."
-ln -s .config/nvim/init.vim ~/.config/nvim/init.vm
-ln -s .zshrc ~/.zshrc
-ln -s .gitconfig ~/.gitconfig
+ln -fs .config/nvim/init.vim ~/.config/nvim/init.vm
+ln -fs .zshrc ~/.zshrc
+ln -fs .gitconfig ~/.gitconfig
 
 echo "Done! 🎉"
 
