d148494645b82ca3dcdb08cdc48db169d69115bc
Alex Taylor <alex.taylor@clio.com>
2022-01-26 10:44:41 -0800

    Better bash

--- setup.sh
+++ setup.sh
@@ -8,14 +8,14 @@ echo "Installing neovim..."
 sudo apt-get -y update && sudo apt-get -y install neovim
 
 echo "Installing and configuring Vundle..."
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone "https://github.com/VundleVim/Vundle.vim.git" "$HOME/.vim/bundle/Vundle.vim"
 # nvim +PluginInstall +qall
 
 echo "Symlinking dotfiles..."
-mkdir -p ~/.config/nvim
-ln -fs .config/nvim/init.vim ~/.config/nvim/init.vm
-ln -fs .zshrc ~/.zshrc
-ln -fs .gitconfig ~/.gitconfig
+mkdir -p "$HOME/.config/nvim"
+ln -fs "$(pwd)/.config/nvim/init.vim" "$HOME/.config/nvim/init.vim"
+ln -fs "$(pwd)/.zshrc" "$HOME/.zshrc"
+ln -fs "$(pwd)/.gitconfig" "$HOME/.gitconfig"
 
 echo "Done! 🎉"
 
