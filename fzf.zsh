ccad506b9007097b6f0547f791aca508e916fa5d
Alex Taylor <alex@alextaylor.ca>
2022-02-18 12:02:17 -0800

    Fix fzf config

--- fzf.zsh
+++ fzf.zsh
@@ -1,16 +1,10 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.vim/bundle/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$HOME/.vim/bundle/fzf/shell/key-bindings.zsh"
 
+export LANG=en_US.UTF-8 # Fixes a perl warning on codespaces, since LANG is en_CA.UTF-8 which isn't present
 export FZF_CTRL_R_OPTS='--height 40% --layout=reverse --color=light'
-
