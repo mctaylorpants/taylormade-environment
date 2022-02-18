c43545da688db5d027272c6527a8687a839142b1
Alex Taylor <alex@alextaylor.ca>
2022-02-18 14:08:17 -0800

    Update fzf binary path

--- fzf.zsh
+++ fzf.zsh
@@ -4,7 +4,7 @@
 
 # Key bindings
 # ------------
-source "$HOME/.vim/bundle/fzf/shell/key-bindings.zsh"
+source "$HOME/.local/share/nvim/plugged/fzf/shell/key-bindings.zsh"
 
 export LANG=en_US.UTF-8 # Fixes a perl warning on codespaces, since LANG is en_CA.UTF-8 which isn't present
 export FZF_CTRL_R_OPTS='--height 40% --layout=reverse --color=light'
